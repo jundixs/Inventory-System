6d4f49cc20cbc0d18b627abeb12f74ecb21c88d5
jundixs <julixpepino3@gmail.com>
2023-12-20 13:13:57 +0800

    da

--- Inventory System/LoginForm.Designer.cs
+++ Inventory System/LoginForm.Designer.cs
@@ -77,11 +77,11 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.White;
-            this.label3.Location = new System.Drawing.Point(67, 18);
+            this.label3.Location = new System.Drawing.Point(91, 16);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(209, 17);
+            this.label3.Size = new System.Drawing.Size(125, 17);
             this.label3.TabIndex = 6;
-            this.label3.Text = "INVENTORY MANGEMENT SYSTEM";
+            this.label3.Text = "CEBU InventoryPro";
             // 
             // pictureBox2
             // 
